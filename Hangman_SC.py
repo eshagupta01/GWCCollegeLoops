cfa3d8333d2902af9239b5a2deac75b2993477cd
riasaxena <58662972+riasaxena@users.noreply.github.com>
2021-10-19 21:27:58 -0700

    Update Hangman_SC.py

--- Hangman_SC.py
+++ Hangman_SC.py
@@ -1,12 +1,23 @@
-#Hangman Starter Code
-
+#Hangman
+def guessing(listofguess, list_char):
+    print("finish later")
+def search(searching, list_char, listofguess):
+    pprint("finish later")
+def strip_input(prompt):
+     string = input(prompt)
+     string = string.strip()
+     string = string.lower()
+     return string
 def print_list(log):
     for x in log:
         print(x, end = '\t')
     print()
+
     
 if __name__ == "__main__":
-    string = input("Enter a word to guess:")
-    guess = input("Enter a character to guess: ")
-    print("You lost, the word was : " + string)
-    print("You won!")
+    print("finish later")
+    #set up the game
+
+    #play the game
+
+    #check ending condition
